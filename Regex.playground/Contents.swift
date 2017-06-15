f377892657176bbac51a24b78ccc9acb28b34cf4
libowen <libowen@cct.cn>
2017-06-15 10:17:53 +0800

    优化正则，匹配度增加，调整页面顺序

--- Regex.playground/Contents.swift
+++ Regex.playground/Contents.swift
@@ -31,7 +31,7 @@ func listMatches(pattern: String, inString string: String) -> [String] {
     return matches.map {
         let range = $0.range
         let matchString = (string as NSString).substring(with: range)
-        print("matchString: \(matchString)")
+        //print("matchString: \(matchString)")
         return matchString
     }
 }
@@ -97,83 +97,15 @@ listMatches(pattern: chinesePattern, inString: chineseString)
 //let htmlString = "<table class=\"wikitable\">        <tr>abc</tr>This is an example <strong>html</strong> string.</table>;<strong>html</strong>"
 // 带换行的html 标签
 let htmlString = "<tr>\n<th>英文术语</th><th>中文翻译</th><th>详细解释\n</th></tr>"
-//let pattern = "<([a-z][a-z0-9]*)\\b[^>]*>(.*?)</\\1>"
+let pattern = "<([a-z][a-z0-9]*)\\b[^>]*>(.*?)</\\1>"  // 匹配任意html tag
 //let pattern = "<(table)*\\b[^>]*>\\s(.*?)</\\1>"
 //let pattern = "<(table)[^>]*>(.*?)</\\1>"
 //let pattern = "<(table)\\b class=\"wikitable\">"
-// 注意html标签是有换行的（\n），正则匹配时要注意
+/* 注意html标签是有换行的（\n），正则匹配时要注意 */
 //let trPattern = "<(tr)>([\\s\\S]*?)</\\1>"
-let trPattern = ">(.+?)<"
-listMatches(pattern: trPattern, inString: htmlString)
-// 处理第二个td
-let tdChineseString = "<td><a href=\"/wiki/%E5%AD%98%E6%AC%BE%E8%AF%81\" title=\"存款证\">存款证</a></td>"
+//let trPattern = ">(.+?)<"
+listMatches(pattern: pattern, inString: htmlString)
 
-let tdChinesePattern = "[\u{4e00}-\u{9fa5}]+" // 匹配中文可行
-let tdChineseResult = listMatches(pattern: tdChinesePattern, inString: tdChineseString)
-
-// 处理第三个td
-/**
- 问题：
- 1、CFA 协会]](CFA Institute)]](前身是AIMR(Association for Investment Management and Research)，于2004年五月正式易名)颁发的专业称号，用以证实投资专业人士的实力及诚信。应考生必须通过三级考试，考核的范围包括道德与专业标准、投资工具、资产估值及投资组合管理。（这种匹配不对）
- 2、美国注册公开会计师协会（American Institute of Certified Public Accountants) 颁发的专业称号，应考生必须通过一次考试，并符合工作经验要求（这种匹配不对）
- 3、//"[^>].*[^<]"无法匹配">及<"???
- 解决：正则匹配，在于找到规律，有了规律就有合适的正则
- */
-let tdExplainString = "<td>持有人可收取<a href=\"/wiki/%E5%88%A9%E6%81%AF\" title=\"利息\">利息</a>的存款证书。存款证设有到期日、<a href=\"/wiki/%E5%9B%BA%E5%AE%9A%E5%88%A9%E7%8E%87\" title=\"固定利率\">固定利率</a>，并可以用任何<a href=\"/wiki/%E8%B4%A7%E5%B8%81\" title=\"货币\">货币</a>计价。存款证一般由<a href=\"/wiki/%E5%95%86%E4%B8%9A%E9%93%B6%E8%A1%8C\" title=\"商业银行\">商业银行</a>发行</td>"
-//let tdPattern = "<([a-z][a-z0-9]*)\\b[^>]*>([\u{4e00}-\u{9fa5}]+)</\\1>"
-//let tdPattern = "<(\\S*?)[^>]*>.*?</>|<.*? />"
-let tdExplainPattern = "[\u{4e00}-\u{9fa5}]+([，。、；]*)[\u{4e00}-\u{9fa5}]+" // 匹配中文+标点符号
-let tdExplainResult = listMatches(pattern: tdExplainPattern, inString: tdExplainString)
-var newtdResult: [String] = []
-for string in tdExplainResult {
-    if newtdResult.contains(string) == false {
-        newtdResult.append(string)
-    }
-}
-print("newtdResult: \(newtdResult)")
-
-// 处理第一个td
-//let tdEnglishString = "<td><a href=\"/w/index.php?title=CEDEL&amp;action=edit\" class=\"new\" title=\"CEDEL\">CEDEL</a></td>"
-//let tdEnglishString = "<td><a href=\"/w/index.php?title=Call_Loan_Rate&amp;action=edit\" class=\"new\" title=\"Call Loan Rate\">Call Loan Rate (Call-Loan Rate)</a></td>"
-//let tdEnglishString = "<td><a href=\"/w/index.php?title=%E6%80%BB%E8%B5%84%E6%9C%AC%EF%BC%8C%E6%80%BB%E5%B8%82%E5%80%BC&amp;action=edit\" class=\"new\" title=\"总资本，总市值\">总资本，总市值</a></td>"
-//let tdEnglishString = "<td><a href=\"/w/index.php?title=%E9%80%89%E5%87%BA%E6%9C%80%E6%9C%89%E5%88%A9%E7%9A%84%EF%BC%88%E6%8A%95%E8%B5%84%E7%AD%89%EF%BC%89&amp;action=edit\" class=\"new\" title=\"选出最有利的（投资等）\">选出最有利的（投资等）</a></td>"
-//let tdEnglishString = "<td>持有人、可收取<a href=\"/wiki/%E5%88%A9%E6%81%AF\" title=\"利息\">利息</a>的存款证书。存款证设有到期日、<a href=\"/wiki/%E5%9B%BA%E5%AE%9A%E5%88%A9%E7%8E%87\" title=\"固定利率\">固定利率</a>，并可以用任何<a href=\"/wiki/%E8%B4%A7%E5%B8%81\" title=\"货币\">货币</a>计价。存款证一般由<a href=\"/wiki/%E5%95%86%E4%B8%9A%E9%93%B6%E8%A1%8C\" title=\"商业银行\">商业银行</a>发行</td>"
-let tdEnglishString = "<td>CFA 协会]](CFA Institute)]](前身是AIMR(Association for Investment Management and Research)，于2004年五月正式易名)颁发的专业称号，用以证实<a href=\"/wiki/%E6%8A%95%E8%B5%84\" title=\"投资\">投资</a>专业人士的实力及<a href=\"/wiki/%E8%AF%9A%E4%BF%A1\" title=\"诚信\">诚信</a>。应考生必须通过三级考试，考核的范围包括道德与专业标准、投资工具、<a href=\"/wiki/%E8%B5%84%E4%BA%A7\" title=\"资产\">资产</a>估值及<a href=\"/wiki/%E6%8A%95%E8%B5%84%E7%BB%84%E5%90%88%E7%AE%A1%E7%90%86\" title=\"投资组合管理\">投资组合管理</a>。</td>"
-//let tdEnglishPattern = ">(\\w+)(\\s+)?(\\w+)<" //只能匹配两个单词
-//let tdEnglishPattern = ">[([(\\w+（）)、，-])(\\s+)]+<"  //匹配多个单词，包含连接符（-）
-//let tdEnglishPattern = ">(.*?)<" // 匹配><之间的所有字符
-let tdEnglishPattern = "(?<=>)(.*?)(?=<)" // 匹配><之间的所有字符
-let tdEnglishResult = listMatches(pattern: tdEnglishPattern, inString: tdEnglishString)
-//let wordPattern = "[^>].*[^<]"
-//let wordResult = listMatches(pattern: wordPattern, inString: tdEnglishResult[0])
-
-// ???
-//let tdString = "<td><a href=\"/w/index.php?title=%E9%80%89%E5%87%BA%E6%9C%80%E6%9C%89%E5%88%A9%E7%9A%84%EF%BC%88%E6%8A%95%E8%B5%84%E7%AD%89%EF%BC%89&amp;action=edit\" class=\"new\" title=\"选出最有利的（投资等）\">选出最有利的（投资等）</a></td>"
-let tdString = ">及<"
-//let tdPattern = "[^>][([(\\w+（）)，-])(\\s+)]+[^<]"
-//let tdPattern = "[^>].*[^<]" //"[^>].*[^<]"无法匹配">及<"???
-let tdPattern = "(?<=>).*(?=<)"
-let tdResult = listMatches(pattern: tdPattern, inString: tdString)
-
-// 无序去重
-var tempArray: [String] = ["a", "b", "b", "c", "d", "d"]
-var tempDic: [String: String] = [:]
-var result1: [String] = []
-//
-
-for string in tempArray {
-    tempDic[string] = string
-}
-print("tempDic: \(tempDic)")
-result1 = [String](tempDic.keys)
-// 有序去重
-var result2: [String] = []
-for string in tempArray {
-    if result2.contains(string) == false {
-        result2.append(string)
-    }
-}
-print("result2: \(result2)")
 
 
 
@@ -309,6 +241,123 @@ listMatches(pattern: "t[a-h]", inString: theVowels)
  */
 let notClasses = "tim tam tum tom tem"
 listMatches(pattern: "t[^oa]m", inString: notClasses)
+
+
+///
+// 不带换行的html标签
+//let tableString = "<p>This is an example <strong>html</strong> string.</p>"
+let tableString = "<table class=\"wikitable\">        <tr>abc</tr>This is an example <strong>html</strong> string.</table>;<strong>html</strong>"
+// 带换行的html 标签
+//let tableString = "<tr>\n<th>英文术语</th><th>中文翻译</th><th>详细解释\n</th></tr>"
+let tablePattern = "<([a-z][a-z0-9]*)\\b[^>]*>(.*?)</\\1>"
+//let tablePattern = "<(table)*\\b[^>]*>\\s(.*?)</\\1>"
+//let tablePattern = "<(table)[^>]*>(.*?)</\\1>"
+//let tablePattern = "<(table)\\b class=\"wikitable\">"
+/* 注意html标签是有换行的（\n），正则匹配时要注意 */
+//let trPattern = "<(tr)>([\\s\\S]*?)</\\1>"
+let trPattern = ">(.+?)<"
+listMatches(pattern: tablePattern, inString: tableString)
+
+
+
+// 处理第二个td
+let tdChineseString = "<td><a href=\"/wiki/%E5%AD%98%E6%AC%BE%E8%AF%81\" title=\"存款证\">存款证</a></td>"
+
+let tdChinesePattern = "[\u{4e00}-\u{9fa5}]+" // 匹配中文可行
+let tdChineseResult = listMatches(pattern: tdChinesePattern, inString: tdChineseString)
+
+// 处理第三个td
+/**
+ 问题：
+ 1、CFA 协会]](CFA Institute)]](前身是AIMR(Association for Investment Management and Research)，于2004年五月正式易名)颁发的专业称号，用以证实投资专业人士的实力及诚信。应考生必须通过三级考试，考核的范围包括道德与专业标准、投资工具、资产估值及投资组合管理。（这种匹配不对）
+ 2、美国注册公开会计师协会（American Institute of Certified Public Accountants) 颁发的专业称号，应考生必须通过一次考试，并符合工作经验要求（这种匹配不对）
+ 3、//"[^>].*[^<]"无法匹配">及<"???
+ 解决：正则匹配，在于找到规律，有了规律就有合适的正则
+ */
+//let tdExplainString = "<td>持有人可收取<a href=\"/wiki/%E5%88%A9%E6%81%AF\" title=\"利息\">利息</a>的存款证书。存款证设有到期日、<a href=\"/wiki/%E5%9B%BA%E5%AE%9A%E5%88%A9%E7%8E%87\" title=\"固定利率\">固定利率</a>，并可以用任何<a href=\"/wiki/%E8%B4%A7%E5%B8%81\" title=\"货币\">货币</a>计价。存款证一般由<a href=\"/wiki/%E5%95%86%E4%B8%9A%E9%93%B6%E8%A1%8C\" title=\"商业银行\">商业银行</a>发行</td>"
+let tdExplainString = "<td>实现<a href=\"/wiki/%E5%88%A9%E6%B6%A6\" title=\"利润\">利润</a>运用的资金总额<br>业务运用的所有资产的价值<br><a href=\"/wiki/%E5%9B%BA%E5%AE%9A%E8%B5%84%E4%BA%A7\" title=\"固定资产\">固定资产</a>+周转资金<br><a href=\"/wiki/%E6%80%BB%E8%B5%84%E4%BA%A7\" title=\"总资产\">总资产</a> - <a href=\"/wiki/%E6%B5%81%E5%8A%A8%E8%B4%9F%E5%80%BA\" title=\"流动负债\">流动负债</a>"
+//let tdPattern = "<([a-z][a-z0-9]*)\\b[^>]*>([\u{4e00}-\u{9fa5}]+)</\\1>"
+//let tdPattern = "<(\\S*?)[^>]*>.*?</>|<.*? />"
+//let tdExplainPattern = "[\u{4e00}-\u{9fa5}]+([，。、；]*)[\u{4e00}-\u{9fa5}]+" // 匹配中文+标点符号
+let tdExplainPattern = "(?<=>).*?(?=<)|(?<=>).*?(?=\\n)"
+let tdExplainResult = listMatches(pattern: tdExplainPattern, inString: tdExplainString)
+var newtdResult: [String] = []
+for string in tdExplainResult {
+    if newtdResult.contains(string) == false {
+        newtdResult.append(string)
+    }
+}
+print("newtdResult: \(newtdResult)")
+
+// 处理第一个td
+//let tdEnglishString = "<td><a href=\"/w/index.php?title=CEDEL&amp;action=edit\" class=\"new\" title=\"CEDEL\">CEDEL</a></td>"
+//let tdEnglishString = "<td><a href=\"/w/index.php?title=Call_Loan_Rate&amp;action=edit\" class=\"new\" title=\"Call Loan Rate\">Call Loan Rate (Call-Loan Rate)</a></td>"
+//let tdEnglishString = "<td><a href=\"/w/index.php?title=%E6%80%BB%E8%B5%84%E6%9C%AC%EF%BC%8C%E6%80%BB%E5%B8%82%E5%80%BC&amp;action=edit\" class=\"new\" title=\"总资本，总市值\">总资本，总市值</a></td>"
+//let tdEnglishString = "<td><a href=\"/w/index.php?title=%E9%80%89%E5%87%BA%E6%9C%80%E6%9C%89%E5%88%A9%E7%9A%84%EF%BC%88%E6%8A%95%E8%B5%84%E7%AD%89%EF%BC%89&amp;action=edit\" class=\"new\" title=\"选出最有利的（投资等）\">选出最有利的（投资等）</a></td>"
+//let tdEnglishString = "<td>持有人、可收取<a href=\"/wiki/%E5%88%A9%E6%81%AF\" title=\"利息\">利息</a>的存款证书。存款证设有到期日、<a href=\"/wiki/%E5%9B%BA%E5%AE%9A%E5%88%A9%E7%8E%87\" title=\"固定利率\">固定利率</a>，并可以用任何<a href=\"/wiki/%E8%B4%A7%E5%B8%81\" title=\"货币\">货币</a>计价。存款证一般由<a href=\"/wiki/%E5%95%86%E4%B8%9A%E9%93%B6%E8%A1%8C\" title=\"商业银行\">商业银行</a>发行</td>"
+let tdEnglishString = "<td>CFA 协会]](CFA Institute)]](前身是AIMR(Association for Investment Management and Research)，于2004年五月正式易名)颁发的专业称号，用以证实<a href=\"/wiki/%E6%8A%95%E8%B5%84\" title=\"投资\">投资</a>专业人士的实力及<a href=\"/wiki/%E8%AF%9A%E4%BF%A1\" title=\"诚信\">诚信</a>。应考生必须通过三级考试，考核的范围包括道德与专业标准、投资工具、<a href=\"/wiki/%E8%B5%84%E4%BA%A7\" title=\"资产\">资产</a>估值及<a href=\"/wiki/%E6%8A%95%E8%B5%84%E7%BB%84%E5%90%88%E7%AE%A1%E7%90%86\" title=\"投资组合管理\">投资组合</a>管理。\n</td>"
+//let tdEnglishPattern = ">(\\w+)(\\s+)?(\\w+)<" //只能匹配两个单词
+//let tdEnglishPattern = ">[([(\\w+（）)、，-])(\\s+)]+<"  //匹配多个单词，包含连接符（-）
+//let tdEnglishPattern = ">(.*?)<" // 匹配'><'之间的所有字符(不含换行\n,)(包含><)
+//let tdEnglishPattern = "(?<=>)(.*?)(?=<)" // 匹配'><'之间的所有字符(不含换行\n,)(不包含><,)
+let tdEnglishPattern = "(?<=>).*?(?=<)|(?<=>).*?(?=\\n)" //匹配'><'或'>\n'之间的所有字符(不包含><,>\n)
+let tdEnglishResult = listMatches(pattern: tdEnglishPattern, inString: tdEnglishString)
+//let wordPattern = "[^>].*[^<]"
+//let wordResult = listMatches(pattern: wordPattern, inString: tdEnglishResult[0])
+
+// ???
+/**
+ ?<= 
+ ?=
+ */
+//let tdString = "<td><a href=\"/w/index.php?title=%E9%80%89%E5%87%BA%E6%9C%80%E6%9C%89%E5%88%A9%E7%9A%84%EF%BC%88%E6%8A%95%E8%B5%84%E7%AD%89%EF%BC%89&amp;action=edit\" class=\"new\" title=\"选出最有利的（投资等）\">选出最有利的（投资等）</a></td>"
+
+let tdString = ">及<"
+//let tdPattern = "[^>][([(\\w+（）)，-])(\\s+)]+[^<]"
+//let tdPattern = "[^>].*[^<]" //"[^>].*[^<]"无法匹配">及<"???
+let tdPattern = "(?<=>).*(?=<)" //
+var tdResult = listMatches(pattern: tdPattern, inString: tdString)
+
+tdResult = listMatches(pattern: "(?=>).{1,2}", inString: ">abcd<")
+
+tdResult = listMatches(pattern: "(?!>).{1,2}", inString: ">abcd<")
+
+
+/// 数组去重
+// 无序去重
+var tempArray: [String] = ["a", "b", "b", "c", "d", "d"]
+var tempDic: [String: String] = [:]
+var result1: [String] = []
+//
+
+for string in tempArray {
+    tempDic[string] = string
+}
+print("tempDic: \(tempDic)")
+result1 = [String](tempDic.keys)
+// 有序去重
+var result2: [String] = []
+for string in tempArray {
+    if result2.contains(string) == false {
+        result2.append(string)
+    }
+}
+print("result2: \(result2)")
+
+
+/// Optional type
+var someString: String
+someString = "abc"
+print(someString)
+
+var someOptionalString: String?
+someOptionalString = "abc"
+print(someOptionalString)
+if someOptionalString == nil {
+    print("nil")
+} else {
+    print(someOptionalString!)
+}
 
 /**
  
